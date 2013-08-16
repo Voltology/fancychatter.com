b9a09b1a538a4c7ae72f8fef15a8547f11e5f7e9
Chris Vuletich <cvuletich@gmail.com>
2013-08-16 15:19:17 -0500

    Update to localinc

--- deploy/.local.inc.php
+++ deploy/.local.inc.php
@@ -38,19 +38,20 @@ switch($_SERVER['HTTP_HOST']) {
   case "dev.m.fancychatter.com":
     define("ENV", "dev");
     break;
-  case "173.203.81.65":
   case "staging.fancychater.com":
   case "staging.api.fancychater.com":
   case "staging.m.fancychater.com":
     define("ENV", "staging");
     break;
+  case "173.203.81.65":
+  case "50.56.5.76":
   case "fancychater.com":
   case "api.fancychater.com":
   case "m.fancychater.com":
     define("ENV", "production");
     break;
   default:
-    //die("An error has occurred.  No environment has been set.");
+    die("An error has occurred.  No environment has been set.");
 }
 
 
@@ -62,7 +63,6 @@ if (ENV === "dev") {
 
   define("API_URL", "http://api.fancychatter/");
   define("DESKTOP_URL", "http://fancychatter/");
-  define("MOBILE_URL", "http://dev.m.fancychatter.com/");
 
   define("JQUERY_VERSION", "1.9.1");
 } else if (ENV === "staging") {
@@ -73,18 +73,16 @@ if (ENV === "dev") {
 
   define("API_URL", "staging.api.fancychatter.com/");
   define("DESKTOP_URL", "staging.fancychatter.com/");
-  define("MOBILE_URL", "staging.m.fancychatter.com/");
 
   define("JQUERY_VERSION", "1.9.1");
 } else if (ENV === "production") {
-  define("DB_HOST", "localhost");
+  define("DB_HOST", "fc-db01");
   define("DB_NAME", "fancychatter");
   define("DB_USER", "fancychatter");
   define("DB_PASS", "Js4kyNU!LWaq");
 
   define("API_URL", "api.fancychatter.com/");
   define("DESKTOP_URL", "fancychatter.com/");
-  define("MOBILE_URL", "m.fancychatter.com/");
 
   define("JQUERY_VERSION", "1.9.1");
 }
