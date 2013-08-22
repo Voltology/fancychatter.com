404692d2a08a04c19f0fe536605a74368a862c60
Chris Vuletich <cvuletich@gmail.com>
2013-08-22 10:35:42 -0500

    Adding privacy policy

--- deploy/desktop/privacy.php
+++ deploy/desktop/privacy.php
@@ -24,6 +24,128 @@ include("header.php");
   <div class="span12">
     <h2>Privacy Policy</h2>
     <p>
+      <p><strong>1. Types of Information We Collect</strong></p>
+
+      <p>We will collect information, including Personal Information and Non-Identifying Information, when you interact with us and the site, for example when you:
+        <ul>
+          <li>access or use the site;</li>
+          <li>register, subscribe, or create an account with FancyChatter;</li>
+          <li>open or respond to our e-mails;</li>
+          <li>visit any page online that displays our ads or content;</li>
+          <li>connect or link to any site via social networking sites;</li>
+          <li>post comments to Online Communities; and</li>
+          <li>provide information to our Vendors.</li>
+        </ul>
+      </p>
+      <p>This Privacy Statement does not apply to the collection of information in any way other than as listed above.</p>
+      <p><strong>2. Your Choices</strong></p>
+
+      <p>We think that you benefit from a more personalized experience when we know more about you and what you like. However, you can limit the information you provide to FancyChatter, and you can limit the communications that FancyChatter sends to you.</p>
+
+      <p>You can manage your email and notice preferences by logging into your account through the FancyChatter website, entering the “My Stuff” section, and selecting “My Email Subscriptions,” or via the FancyChatter mobile application, by selecting “Project” in your account options and then selecting “Subscriptions.” You can also manage your subscriptions by following subscription management instructions contained in the commercial e-mails that we send you. You may choose to subscribe to some types of messages, and may choose to unsubscribe from others. You may update your subscription preferences at any time. Please note that even if you unsubscribe from promotional email messages, we may still need to contact you with important transactional information related to your account and your purchases. For example, even if you have unsubscribed from our promotional email messages, we will still send you confirmations when you make purchases on the site.</p>
+
+      <p>You may manage how your browser handles Cookies by adjusting its privacy and security settings. Browsers are different, so refer to instructions related to your browser to learn about cookie-related and other privacy and security settings that may be available. You can opt-out of being targeted by certain Third-Party Ad-Servers online using the National Advertising Initiative’s Opt-Out Tool, or on TRUSTe’s site.</p>
+
+      <p>You may manage how your mobile device and mobile browser share certain Device Data with FancyChatter, as well as how your mobile browser handles Cookies by adjusting the privacy and security settings on your mobile device. Please refer to instructions provided by your mobile service provider or the manufacturer of your device to learn how to adjust your settings.</p>
+
+      <p>You may also manage the sharing of certain Personal Information with us when you connect with us through social networking platforms or applications. Please refer to the privacy policy and settings of the social networking website or application to determine how you may adjust our permissions and manage the interactivity between FancyChatter and your social networking account or your mobile device.</p>
+
+      <p>If you wish to opt out of receiving offers directly from our Business Partners, you can follow the opt-out instructions in the emails that they send you.</p>
+
+      <p><strong>3. How FancyChatter Uses Information</strong></p>
+
+      <p>We (or our Vendors on our behalf), use information collected as described in this Privacy Statement to:</p>
+      <ul>
+        <li>Operate, maintain and improve the site and our services;</li>
+        <li>Provide you with personalized ads and offers for products and services from us and participating Business Partners;</li>
+        <li>Facilitate and fulfill orders placed on the site – for example, for FancyChatter vouchers and other goods and services, including tracking redemption;</li>
+        <li>Evaluate your eligibility for certain types of offers, products or services that may be of interest to you, and analyze advertising effectiveness;</li>
+        <li>Answer your questions and respond to your requests;</li>
+        <li>Perform analytics and conduct customer research;</li>
+        <li>Communicate and provide additional information that may be of interest to you about FancyChatter and our Business Partners, sometimes by combining your information with information from Other Sources;</li>
+        <li>Send you reminders, technical notices, updates, security alerts, support and administrative messages, service bulletins, marketing messages, and requested information, including on behalf of Business Partners;</li>
+        <li>Administer rewards, surveys, sweepstakes, contests, or other promotional activities or events sponsored by us or our Business Partners;</li>
+        <li>Manage our everyday business needs, such as administration of the site, forum management, fulfillment, analytics, fraud prevention, and enforcement of our corporate reporting obligations and Terms of Use, or to comply with the law;</li>
+        <li>Allow you to apply for a job, post a video or sign up for special offers from merchants, Business Partners, or third parties through the site, and to</li>
+        <li>Enhance other information we have about you directly or from Other Sources to help us better understand you and determine your interests.</li>
+        <li>We also may use information collected as described in this Privacy Statement with your consent or as otherwise required or permitted by law.</li>
+      </ul>
+
+      <p><strong>4. When and Why FancyChatter Discloses Information</strong></p>
+
+      <p>We (or our Vendors on our behalf) may share your Personal Information as required or permitted by law:</p>
+      <ul>
+        <li>with any FancyChatter Affiliate who may only use the Personal Information for the purposes described in this Privacy Statement;</li>
+        <li>with our Vendors to provide services for us and who are required to protect the Personal Information;</li>
+        <li>to report or collect on debts owed to us or our Business Partners;</li>
+        <li>with relevant Business Partners:</li>
+        <li>with whom we jointly offer products and services;</li>
+        <li>to facilitate a direct relationship with you, including in connection with any program we administer on behalf of the Business Partner;</li>
+        <li>to enable electronic communications with you as part of purchase, a sponsored reward, offer, contest, program, or other activity in which you have elected to participate;</li>
+        <li>to the extent you have purchased or redeemed a FancyChatter voucher, goods or services offered by a Business Partner or participated in an offer, rewards, contest or other activity or program sponsored or offered through FancyChatter on behalf of that Business Partner;</li>
+        <li>with a purchaser of FancyChatter or any of the FancyChatter Affiliates (or their assets);</li>
+        <li>to comply with legal orders and government requests, or as needed to support auditing, compliance, and corporate governance functions;</li>
+        <li>to combat fraud or criminal activity, and to protect our rights or those of our Affiliates, users, and Business Partners, or as part of legal proceedings affecting FancyChatter;</li>
+        <li>in response to a subpoena, or similar legal process, including to law enforcement agencies, regulators, and courts in the United States and other countries where we operate; or
+        with your consent.</li>
+      </ul>
+      <p>We encourage Business Partners to adopt and post privacy policies. However, their use of Personal Information obtained through FancyChatter is governed by their privacy policies and is not subject to our control.</p>
+
+      <p>We may also disclose Non-Identifiable Information:</p>
+      <ul>
+        <li>for the same reasons we might share Personal Information;</li>
+        <li>with Business Partners for their own analysis and research;</li>
+        <li>to facilitate targeted content and ads; or</li>
+        <li>with Third-Party Ad-Servers to place our ads and/or ads of our merchants or Business Partners on the site and on third-party sites, and to analyze the effectiveness of those ads.</li>
+      </ul>
+      <p>We do not share Personal Information with Third-Party Ad-Servers; however, Third-Party Ad-Servers may automatically collect Non-Identifying Information about your visit to the site and other websites, your device address, your Internet Service Provider and the browser you use to visit the site. They do this by using Cookies, clear gifs and other technologies. Information collected may be used, among other things, to deliver advertising targeted to your interests and to better understand the usage and visits to the site and the other websites tracked by these third parties. This Privacy Statement does not cover the collection methods or use of the information collected by Third-Party Ad-Servers, and FancyChatter is not responsible for Cookies or clear gifs in third party ads. We encourage you to review the privacy policies or statements of these third party advertising companies to learn more about their use of Cookies and other technologies. If you would like more information about this practice and to know your choices about not having this information used by third party advertisers, please visit http://www.networkadvertising.org.</p>
+
+      <p><strong>5. How FancyChatter Collects Information</strong></p>
+
+      <p>You provide us with your Personal Information when you register, subscribe, create an account, purchase or redeem FancyChatter vouchers, or otherwise when you provide us with your Personal Information during your interaction with the site. We also collect Personal Information when you contact us online for customer service and other support using self-help tools, such as email, text, or by posting to an Online Community.</p>
+
+      <p>We also receive Personal Information and other online and offline information from Other Sources. FancyChatter will use such information in accordance with applicable laws. Such information, when combined with Personal Information collected as provided in this Privacy Statement, will also be handled in accordance with this Privacy Statement. We also use cookies, tags, web beacons, local shared objects, files, tools and programs to keep records, store your preferences, improve our advertising, and collect Non-Identifying Information, including Device Data and your interaction with the site and our Business Partners' web sites.</p>
+
+      <p>We use Cookies that contain serial numbers that allow us to connect your site activity with other information we store about you in your profile or as related to your interactions with the site. We use session cookies on a temporary basis, such as to manage your view of pages on the site. We use persistent cookies for a number of purposes, such as retrieving certain information you have previously provided (for example, your user id if you asked to be remembered), and storing your preferences (for example, the cities for which you want to be shown FancyChatter vouchers). Information from Cookies also tells us about the website you were visiting before you came to the site and the website you visit after you leave the site.</p>
+
+      <p>When you access these pages or open email messages, we use Pixel Tags and Web Beacons to generate a notice of that action to us, or our Vendors. These tools allow us to measure response to our communications and improve the site’ pages and promotions.</p>
+
+      <p>Device Data may be collected when your device interacts with the site and FancyChatter, even if you are not logged into the site using your device. If you have questions about the security and privacy settings of your mobile device, please refer to instructions from your mobile service provider or the manufacturer of your device to learn how to adjust your settings.</p>
+
+      <p><strong>6. Security of Personal Information</strong></p>
+
+      <p>FancyChatter has implemented an information security program that contains administrative, technical and physical controls that are designed to reasonably safeguard Personal Information. For example, we use industry-standard encryption technology to secure Financial Account Information.</p>
+
+      <p><strong>7. Accessing and Correcting Personal Information</strong></p>
+
+      <p>You can access, update and delete your , Financial Account Information, and the other information you provided to us in your profile by logging into your account and accessing the “My Account” section of the site or sending us an email at privacy@fancychatter.com. Keeping your Personal Information current helps ensure that we, our Affiliates and Business Partners offer you the offers that are most relevant to you.</p>
+
+      <p>If you want to close your FancyChatter account or have other questions or requests, please contact us. While we are ready to assist you in managing your subscriptions, closing your account, and removing your active profile, we cannot always delete records of past interactions and transactions. For example, we are required to retain records relating to previous purchases on the site for financial reporting and compliance reasons.</p>
+
+      <p>We will retain your Personal Information for as long as your account is active or as needed to provide you services and to maintain a record of your transactions for financial reporting purposes. If you wish to cancel your account or request that we no longer use your Personal Information to provide you services contact us at support@fancychatter.com. We will retain and use your Personal Information as necessary to comply with our legal obligations, resolve disputes, and enforce our agreements.</p>
+
+      <p><strong>8. Social Networks</strong></p>
+
+      <p>Social Community Areas</p>
+
+      <p>The site may be accessible through or contain connections to areas where you may be able to publicly post information, communicate with others such as discussion boards or blogs, review products and merchants, and submit media content. Prior to posting in these areas, please read our Terms of Use carefully. All the information you post may be accessible to anyone with Internet access, and any Personal Information you include in your posting may be read, collected, and used by others. For example, if you post your email address along with a public restaurant review, you may receive unsolicited messages from other parties. You should avoid publicly posting Personal Information or identifying information about third parties.</p>
+
+      <p>Connecting through Social Networks</p>
+
+      <p>FancyChatter offers social networking users the opportunity to interact with friends and to share on social networks. If you are logged into both FancyChatter and a social network, when you use the site’ social networking connection functions, we will link your social network account with your FancyChatter account if the email addresses match. If the email addresses do not match, we ask you if you want to link them and you must validate that you control the accounts. If you are already logged into FancyChatter but not logged into your social network site, when you use the site’ social network connection functions, you will be prompted to enter your social network web site credentials or to sign up for the social network.</p>
+
+      <p>If you are not currently registered as a FancyChatter user and you use the site’s social network connection functions, you will first be asked to enter your social network credentials and then be given the option to register and join FancyChatter. Once you register with FancyChatter and connect with the social network, you will be able to automatically post recent FancyChatter activity back to your social network. Please refer to the privacy settings in your social network account to manage the data that is shared through your account.</p>
+
+      <p>By proceeding through any of the above steps, you grant FancyChatter permission to access all of the elements of your social network profile information that you have made available to be shared and to use it in accordance with the social network’s terms of use and this Privacy Statement.</p>
+
+      <p><strong>9. Privacy Practices of Third Parties</strong></p>
+
+      <p>This Privacy Statement only addresses the use and disclosure of information by FancyChatter through your interaction with the site. Other websites that may be accessible through links from the site may have their own privacy statements and personal information collection, use, and disclosure practices. Our Business Partners may also have their own privacy statements. We encourage you to familiarize yourself with the privacy statements provided by these other parties prior to providing them with information or taking advantage of a sponsored offer or promotion.</p>
+
+      <p><strong>10. Children’s Privacy</strong></p>
+
+      <p>FancyChatter does not intend that any portion of the site will be accessed or used by children under the age of thirteen, and such use is prohibited. The site is designed and intended for adults. By using FancyChatter, you represent that you are at least eighteen years old and understand that you must be at least eighteen years old in order to create an account and purchase the goods or services advertised through the site. We will promptly delete information associated with any account be obtain actual knowledge is associated with a registered user who is not at least eighteen years old.</p>
+
     </p>
   </div>
 </div>
